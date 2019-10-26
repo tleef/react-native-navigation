ea2d852fc4c6ac4b7a3ce1a541d2088e67ea61e5
tleef <tom@tleef.com>
2019-10-26 20:38:56 -0300

    Enable debug logs on example app

--- example/App.tsx
+++ example/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import debug from "debug";
 
 import {NavigationContainer} from "../src"
 import AuthSwitchNav from "./navigators/AuthSwitchNav";
 import Paths from "./constants/Paths";
+
+debug.enable("navigation");
 
 export default function App() {
   return (
